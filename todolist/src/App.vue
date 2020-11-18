23eeda89b904772fdd35537c0511a5b8bab76fb3
Eugene <2422@hanmail.net>
2020-11-18 17:34:32 +0900

    vue TodoList pub & component 분리

---
+++ todolist/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader.vue'
+  import TodoInput from './components/TodoInput.vue'
+  import TodoList from './components/TodoList.vue'
+  import TodoFooter from './components/TodoFooter.vue'
+
+  export default{
+    props:['propsdata'],
+    data(){
+      return {
+        todoItems: []
+      }
+    },
+    methods: {
+      addTodo(todoItem){
+        localStorage.setItem(todoItem,todoItem);
+        this.todoItems.push(todoItem);
+        console.log("Input : "+ todoItem);
+        
+        // 로컬 스토리지에 데이터를 추가하는 로직
+      },
+      clearAll(){
+        localStorage.clear();
+        this.todoItems = [];
+      },
+      removeTodo(todoItem,index){
+        localStorage.removeItem(todoItem);
+        this.todoItems.splice(index,1);
+        
+        console.log("Delete : "+ todoItem);
+      }
+    },
+    components : {
+      'TodoHeader' : TodoHeader,
+      'TodoInput' : TodoInput,
+      'TodoList' : TodoList,
+      'TodoFooter' : TodoFooter
+    },
+    created(){
+      // life cycle 생성 시점에 localStorage가 0이 아닐때 todoTiems(배열)에 data push
+      if(localStorage.length > 0){
+        for(var i = 0 ; i < localStorage.length; i++){
+          // index 순서대로 data push
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width:200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03)
+  }
+
+</style>
---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,90 @@
+<template>
+  <section>
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem , index) in propsdata" :key="todoItem" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </transition-group>
+  </section>
+</template>
+
+<script>
+  export default{
+
+    props: ['propsdata'],
+    methods:{
+      removeTodo(todoItem,index){
+        console.log(todoItem,index);
+        //localStorage.removeItem(todoItem);
+        //this.todoItems.splice(index,1);
+        this.$emit('removeTodo',todoItem,index);
+      }
+    }
+    /* App.vue 로 이동
+    data(){
+      return {
+        todoItems : []
+      }
+    },
+    created(){
+      // life cycle 생성 시점에 localStorage가 0이 아닐때 todoTiems(배열)에 data push
+      if(localStorage.length > 0){
+        for(var i = 0 ; i < localStorage.length; i++){
+          // index 순서대로 data push
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+    */
+    
+  
+  }
+
+  
+
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+
+  li{
+    display: flex;
+    min-height: 50px;
+    height:50px;
+    line-height: 50px;
+    margin:0.5rem 0;
+    padding:0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn{
+    line-height: 45px;
+    color : #62acde;
+    margin-right: 5px;
+  }
+
+  .removeBtn{
+    margin-left: auto;
+    color:#de4343;
+  }
+
+  .list-enter-active, .list-leave-active{
+    transition: all 1s;
+  }
+
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translate(30px);
+  }
+
+</style>
